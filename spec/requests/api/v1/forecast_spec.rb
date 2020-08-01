fcde737e8de03a3eb7f6e3b620ae0602548fc636
Taylor Keller <takeller17@gmail.com>
2020-08-01 11:26:22 -0600

    Adds test for forecast endpoint

---
+++ spec/requests/api/v1/forecast_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Forecast API' do
+  it 'Can retrieve weather for a city' do
+
+    get '/api/v1/forecast?location=denver,co'
+
+    expect(response).to be_successful
+
+    forecast = JSON.parse(response.body, symbolize_names: true)
+
+    expect(forecast[:data].keys).to match_array([:current_weather]) 
+  end
+end
