0c2c1163e7447234b6240104d0e8d70634c8f53c
Taylor Keller <takeller17@gmail.com>
2020-08-02 13:39:57 -0600

    Adds serialization of Forecast attributes in model. Properly working

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::ForecastController < ApplicationController
   def index
     forecast_results = ForecastResults.new
     forecast = forecast_results.forecast(params[:location])
-    binding.pry
     render json: ForecastSerializer.new(forecast)
   end
 end
--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,2 +1,6 @@
 class Forecast < ApplicationRecord
+  serialize :location, JSON
+  serialize :current, JSON
+  serialize :daily, JSON
+  serialize :hourly, JSON
 end
