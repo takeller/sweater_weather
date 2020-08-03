9d20b490a4440d1f3b91562be72d75eee2cf5606
Taylor Keller <takeller17@gmail.com>
2020-08-03 09:17:21 -0600

    Adds test for getting current forecast of start location

---
+++ spec/requests/api/v1/trails_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Trails Endpoint' do
+  it 'Can get the current forecast for the start location' do
+    get '/api/v1/trails?location=denver,co'
+
+    expect(response).to be_successful
+
+    current_forecast = JSON.parse(response.body, symbolize_names: true)
+    current_forecast = current_forecast[:data][:attributes]
+    
+    expect(current_forecast[:location].present?).to eq(true)
+    expect(current_forecast[:forecast].present?).to eq(true)
+    expect(current_forecast[:forecast][:summary].present?).to eq(true)
+    expect(current_forecast[:forecast][:temperature].present?).to eq(true)
+  end
+end
