0408f32374440fe0f32f23f61a47ecaa6ad6934f
Taylor Keller <takeller17@gmail.com>
2020-08-03 09:44:58 -0600

    Adds test for trail details

--- spec/requests/api/v1/trails_spec.rb
+++ spec/requests/api/v1/trails_spec.rb
@@ -14,4 +14,19 @@ describe 'Trails Endpoint' do
     expect(current_forecast[:forecast][:summary].present?).to eq(true)
     expect(current_forecast[:forecast][:temperature].present?).to eq(true)
   end
+
+  it 'Can get the trail details for each trail' do
+    get '/api/v1/trails?location=denver,co'
+
+    expect(response).to be_successful
+
+    trails = JSON.parse(response.body, symbolize_names: true)
+    trails = trails[:data][:attributes]
+
+    expect(trails[:trails][0][:name].present?).to eq(true)
+    expect(trails[:trails][0][:summary].present?).to eq(true)
+    expect(trails[:trails][0][:difficulty].present?).to eq(true)
+    expect(trails[:trails][0][:location].present?).to eq(true)
+    expect(trails[:trails][0][:distance_to_trail].present?).to eq(true)
+  end
 end
