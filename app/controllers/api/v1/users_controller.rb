2f3ed4aca2ba33a8b7b4d5b0c1f23257408028a5
Taylor Keller <takeller17@gmail.com>
2020-08-03 14:39:45 -0600

    Finish registration

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,16 @@
+require 'securerandom'
+
 class Api::V1::UsersController < ApplicationController
 
   def create
     json = JSON.parse(request.body.read, symbolize_names: true)
-    User.new(json)
+    api_key = SecureRandom.uuid
+    user = User.new(json)
+    user.api_key = api_key
+    if user.save
+      render json: UserSerializer.new(user), status: 201
+    elsif !user.save
+      render json: user.errors.full_messages.to_sentence, status: 400
+    end
   end
 end
--- spec/requests/api/v1/registration_spec.rb
+++ spec/requests/api/v1/registration_spec.rb
@@ -20,4 +20,57 @@ describe 'Registration endpoint' do
     expect(user[:attributes][:email]).to eq(body["email"])
     expect(user[:attributes][:api_key].present?).to eq(true)
   end
+
+  it 'Gets a 400 status code if password is missing' do
+    headers = { "Content-Type" => "application/json", "Accept" => "application/json"}
+    body = {
+      "email" => "whatever@example.com",
+      "password" => "",
+      "password_confirmation" => "password"
+    }
+    post "/api/v1/users", params: body.to_json, headers: headers
+
+    expect(response.status).to eq(400)
+    expect(response.body).to eq("Password can't be blank")
+  end
+
+  it 'Gets a 400 status code if passwords dont match' do
+    headers = { "Content-Type" => "application/json", "Accept" => "application/json"}
+    body = {
+      "email" => "whatever@example.com",
+      "password" => "password",
+      "password_confirmation" => "test"
+    }
+    post "/api/v1/users", params: body.to_json, headers: headers
+
+    expect(response.status).to eq(400)
+    expect(response.body).to eq("Password confirmation doesn't match Password")
+  end
+
+  it 'Gets a 400 status code if email is not unique' do
+    User.create(email: "whatever@example.com", password: "test", password_confirmation: "test")
+    headers = { "Content-Type" => "application/json", "Accept" => "application/json"}
+    body = {
+      "email" => "whatever@example.com",
+      "password" => "password",
+      "password_confirmation" => "password"
+    }
+    post "/api/v1/users", params: body.to_json, headers: headers
+
+    expect(response.status).to eq(400)
+    expect(response.body).to eq("Email has already been taken")
+  end
+
+  it 'Gets a 400 status code if email is empty' do
+    headers = { "Content-Type" => "application/json", "Accept" => "application/json"}
+    body = {
+      "email" => "",
+      "password" => "password",
+      "password_confirmation" => "password"
+    }
+    post "/api/v1/users", params: body.to_json, headers: headers
+
+    expect(response.status).to eq(400)
+    expect(response.body).to eq("Email can't be blank")
+  end
 end
