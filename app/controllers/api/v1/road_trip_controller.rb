85f4dd7586f99d4fa964c3d49b5a3b3ef92f523b
Taylor Keller <takeller17@gmail.com>
2020-08-03 17:36:32 -0600

    Working on refactoring road trip index

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def index
+    road_trip_params = JSON.parse(request.body.read, symbolize_names: true)
     api_key = road_trip_params[:api_key]
     if api_key.nil?
       render json: "Missing API Key", status: 401
+    elsif User.find_by(api_key: api_key).nil?
+      render json: "Invalid API Key", status: 401
     elsif road_trip_params[:origin].present? == false
       render json: "Missing origin location", status: 400
     elsif road_trip_params[:destination].present? == false
       render json: "Missing destination location", status: 400
-    elsif User.find_by(api_key: api_key).nil?
-      render json: "Invalid API Key", status: 401
     elsif User.find_by(api_key: api_key)
       road_trip_results = RoadTripResults.new
       road_trip = road_trip_results.road_trip(road_trip_params.except(:api_key))
