0a8530198bb62a45695f6f18cb76bf7215fa0f1f
Taylor Keller <takeller17@gmail.com>
2020-08-03 17:11:44 -0600

    Road trip sad path testing passing

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,18 +1,19 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def index
-    api_key = params[:road_trip][:api_key]
+    road_trip_params = JSON.parse(request.body.read, symbolize_names: true)
+    api_key = road_trip_params[:api_key]
     if api_key.nil?
       render json: "Missing API Key", status: 401
-    elsif params[:road_trip][:origin].present? == false
+    elsif road_trip_params[:origin].present? == false
       render json: "Missing origin location", status: 400
-    elsif params[:road_trip][:destination].present? == false
+    elsif road_trip_params[:destination].present? == false
       render json: "Missing destination location", status: 400
     elsif User.find_by(api_key: api_key).nil?
       render json: "Invalid API Key", status: 401
     elsif User.find_by(api_key: api_key)
       road_trip_results = RoadTripResults.new
-      road_trip = road_trip_results.road_trip(params[:road_trip].except(:api_key))
+      road_trip = road_trip_results.road_trip(road_trip_params.except(:api_key))
       render json: RoadTripSerializer.new(road_trip)
     else
       render json: "Bad Request", status: 400
--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -5,9 +5,9 @@ describe 'POST /api/v1/road_trip' do
     User.create(email: "whatever@example.com", password: "password", password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
     headers = { "Content-Type" => "application/json", "Accept" => "application/json"}
     body = {
-      "origin": "Denver,CO",
-      "destination": "Pueblo,CO",
-      "api_key": "jgn983hy48thw9begh98h4539h4"
+      "origin" => "Denver,CO",
+      "destination" => "Pueblo,CO",
+      "api_key" => "jgn983hy48thw9begh98h4539h4"
     }
     post "/api/v1/road_trip", params: body.to_json, headers: headers
 
@@ -24,25 +24,54 @@ describe 'POST /api/v1/road_trip' do
   it 'Returns a 401 error if api key is missing' do
     headers = { "Content-Type" => "application/json", "Accept" => "application/json"}
     body = {
-      "origin": "Denver,CO",
-      "destination": "Pueblo,CO",
-      "api_key": "jgn983hy48thw9begh98h4539h4"
+      "origin" => "Denver,CO",
+      "destination" => "Pueblo,CO",
     }
     post "/api/v1/road_trip", params: body.to_json, headers: headers
 
     expect(response.status).to eq(401)
+    expect(response.body).to eq("Missing API Key")
   end
 
   it 'Returns a 401 error if api key is incorrect' do
     User.create(email: "whatever@example.com", password: "password", password_confirmation: "password", api_key: "123456789")
     headers = { "Content-Type" => "application/json", "Accept" => "application/json"}
     body = {
-      "origin": "Denver,CO",
-      "destination": "Pueblo,CO",
-      "api_key": "jgn983hy48thw9begh98h4539h4"
+      "origin" => "Denver,CO",
+      "destination" => "Pueblo,CO",
+      "api_key" => "jgn983hy48thw9begh98h4539h4"
     }
     post "/api/v1/road_trip", params: body.to_json, headers: headers
 
     expect(response.status).to eq(401)
+    expect(response.body).to eq("Invalid API Key")
+  end
+
+  it 'Returns a 400 error if origin is missing' do
+    User.create(email: "whatever@example.com", password: "password", password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
+    headers = { "Content-Type" => "application/json", "Accept" => "application/json"}
+    body = {
+      "origin" => "",
+      "destination" => "Pueblo,CO",
+      "api_key" => "jgn983hy48thw9begh98h4539h4"
+    }
+    post "/api/v1/road_trip", params: body.to_json, headers: headers
+
+    expect(response.status).to eq(400)
+    expect(response.body).to eq("Missing origin location")
+  end
+
+  it 'Returns a 400 error if destination is missing' do
+    User.create(email: "whatever@example.com", password: "password", password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
+    headers = { "Content-Type" => "application/json", "Accept" => "application/json"}
+    body = {
+      "origin" => "Denver,CO",
+      "destination" => "",
+      "api_key" => "jgn983hy48thw9begh98h4539h4"
+    }
+    post "/api/v1/road_trip", params: body.to_json, headers: headers
+
+    expect(response.status).to eq(400)
+    expect(response.body).to eq("Missing destination location")
   end
 end
